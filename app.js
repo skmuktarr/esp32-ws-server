be9951eeb158a09c3e563684ad9579e59ceef923
skmuktarr <89960400+skmuktarr@users.noreply.github.com>
2025-06-24 22:00:51 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,30 @@
+const http = require("http");
+const fs = require("fs");
 const WebSocket = require("ws");
-const wss = new WebSocket.Server({ port: 80 });
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    fs.readFile("index.html", function (err, data) {
+      if (err) {
+        res.writeHead(500);
+        res.end("Error loading HTML");
+      } else {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(data);
+      }
+    });
+  }
+});
+
+const wss = new WebSocket.Server({ server });
 
 wss.on("connection", function connection(ws) {
   console.log("Client connected");
 
   ws.on("message", function incoming(message) {
-    console.log("received: %s", message);
-    // You can optionally broadcast to all clients
-    wss.clients.forEach(function each(client) {
-      if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(message);
-      }
-    });
+    console.log("Received:", message);
+    // Forward or handle messages here
   });
 });
+
+server.listen(80, () => console.log("Server running on port 80"));
